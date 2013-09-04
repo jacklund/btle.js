87843115e06fab5982f2217c1c11326e59bffba4
Jack Lund <jack.lund@geekheads.net>
2013-09-03 20:57:42 -0500

    Added second stream of readings for test

--- test.js
+++ test.js
@@ -28,14 +28,18 @@ conn.connect(connect_opts, function(err) {
   console.log("Got connection, err = " + err);
   // Listen for notifications
   conn.addNotificationListener(0x25, function(buffer) {
-    console.log(buffer);
+    console.log("Temperature, " + buffer.readUInt16LE(0) + ", " + buffer.readUInt16LE(2));
+  });
+
+  conn.addNotificationListener(0x2D, function(buffer) {
+    console.log("Accelerometer, " + buffer.readUInt8(0) + ", " + buffer.readUInt8(1) + ", " + buffer.readUInt8(2));
   });
 
   // Read from handle 0x25
   conn.readHandle(0x25, function(buffer) {
     console.log(buffer);
 
-    // Write a 1 to handle 0x29
+    // Write a 1 to handle 0x29 to turn on the thermometer
     buffer = new Buffer([1]);
     console.log(buffer);
     conn.writeCommand(0x29, buffer);
@@ -48,6 +52,9 @@ conn.connect(connect_opts, function(err) {
 	// Write 0100 to handle 0x26
 	buffer = new Buffer([1, 0]);
 	conn.writeCommand(0x26, buffer);
+
+	conn.writeCommand(0x31, new Buffer([1])); // Turn on accelerometer
+	conn.writeCommand(0x2E, buffer);
       });
     }, 1000);
   });
