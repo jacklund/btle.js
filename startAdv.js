098432b2bf12e241d34c017ee541d8a91ca9f081
Jack Lund <jack.lund@geekheads.net>
2013-12-17 03:33:01 +0000

    Got device info service and read request working

--- startAdv.js
+++ startAdv.js
@@ -1,11 +1,12 @@
 var btle = require('./lib/btle');
+var gatt = require('./lib/gatt');
 var Peripheral = require('./lib/peripheral');
 
 btle.setDebug(true);
 
 // Create the peripheral
 var services = [];
-services.push(createDeviceInfoService('My MFG', '0', '0000-0000', 'v0.01', 'v0.01', 'v0.01', 'My BLE Device'));
+services.push(gatt.createDeviceInfoService('My MFG', '0', '0000-0000', 'v0.01', 'v0.01', 'v0.01', 'My BLE Device'));
 var peripheral = Peripheral.create('My BLE Device', services);
 
 // Start advertising
