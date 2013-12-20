0f7c3646866db69be0fa1360e61af678f3bfcdfd
Jack Lund <jack.lund@geekheads.net>
2013-12-20 04:42:49 +0000

    Characteristic emits write event if value is written

--- startAdv.js
+++ startAdv.js
@@ -3,14 +3,28 @@ var characteristic = require('./lib/characteristic');
 var gatt = require('./lib/gatt');
 var Peripheral = require('./lib/peripheral');
 var service = require('./lib/service');
+var util = require('util');
 
 btle.setDebug(true);
 
-// Create the peripheral
+// Create the services.
 var services = [];
+
+// Device Info service
 services.push(gatt.createDeviceInfoService('My MFG', '0', '0000-0000', 'v0.01', 'v0.01', 'v0.01', 'My BLE Device'));
+
+// Custom service
 var characteristics = [];
-characteristics.push(characteristic.create(null, characteristic.Properties.READ | characteristic.Properties.WRITE, 0xAA01, 0));
+// One read/write characteristic
+var c = characteristic.create(null, characteristic.Properties.READ | characteristic.Properties.WRITE, 0xAA01, 0);
+
+// Called whenever the characteristic value is written
+c.on('write', function(c) {
+  console.log("Characteristic %s written, new value = %s", c.uuid.toString(), util.inspect(c.value));
+});
+characteristics.push(c);
+
+// Create the custom service
 services.push(service.create(null, 0xAA00, null, characteristics, peripheral));
 var peripheral = Peripheral.create('My BLE Device', services);
 
